4a6d49feca35c1c9ad1188ca38983adf8d00c951
JoshyPHP <s9e.dev@gmail.com>
2015-05-24 21:57:56 +0200

    WiP

--- travis/setup-phpbb.sh
+++ travis/setup-phpbb.sh
@@ -14,28 +14,6 @@ set -x
 DB=$1
 TRAVIS_PHP_VERSION=$2
 
-if [ "$TRAVIS_PHP_VERSION" == "5.3" -a "$DB" == "mysqli" ]
-then
-	travis/setup-exiftool.sh
-	travis/setup-unbuffer.sh
-fi
-
-if [ "$DB" == "mariadb" ]
-then
-	travis/setup-mariadb.sh
-fi
-
-if [ "$TRAVIS_PHP_VERSION" != "hhvm" ]
-then
-	travis/setup-php-extensions.sh
-fi
-
-if [ `php -r "echo (int) version_compare(PHP_VERSION, '5.3.19', '>=');"` == "1" ]
-then
-	travis/setup-webserver.sh
-	travis/install-phpbb-test-dependencies.sh
-fi
-
 cd phpBB
 php ../composer.phar install --dev --no-interaction --prefer-source
 cd ..
